a459ee79b6097ee731e84240ee5f3edf83784e26
IvanZakharov <blackmore09218@yandex.ru>
2022-10-15 06:54:05 +0300

    Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя:  ");
+string name = Console.ReadLine();
+if(name.ToLower() == "маша")
+{
+    Console.Write("Ура!!! Это же " );
+    Console.WriteLine(name);
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(name);
+}
