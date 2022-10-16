b69fc7097cbdec5068e19a32a72291ee4c7620bb
IvanZakharov <blackmore09218@yandex.ru>
2022-10-16 07:36:37 +0300

    Программа сравнения двух чисел

---
+++ Homework001/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число: ");
+string a=Console.ReadLine();
+double a1 = double.Parse(a);
+Console.WriteLine("Введите второе число: ");
+string b=Console.ReadLine();
+double b1 = double.Parse(b);
+if(a1>b1)
+{
+    Console.Write("max = "); Console.WriteLine(a1);
+}
+else 
+{
+    if(a1<b1)
+    {
+        Console.Write("max = "); Console.WriteLine(b1);
+    }
+    else
+    {
+        Console.WriteLine("Оба числа равны");
+    }
+}
