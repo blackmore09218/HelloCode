2c114c37df53ffee23b91a1a65535bb3953e951a
IvanZakharov <blackmore09218@yandex.ru>
2022-10-16 22:44:55 +0300

    Добавил условие в виде комментария во все домашние задания по первому семинару

--- Homework001/Program.cs
+++ Homework001/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.WriteLine("Введите первое число: ");
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+Console.WriteLine("Введите первое число: ");
 string a=Console.ReadLine();
 double a1 = double.Parse(a);
 Console.WriteLine("Введите второе число: ");
@@ -6,13 +10,15 @@ string b=Console.ReadLine();
 double b1 = double.Parse(b);
 if(a1>b1)
 {
-    Console.Write("max = "); Console.WriteLine(a1);
+    Console.Write("max = ");
+    Console.WriteLine(a1);
 }
 else 
 {
     if(a1<b1)
     {
-        Console.Write("max = "); Console.WriteLine(b1);
+        Console.Write("max = ");
+        Console.WriteLine(b1);
     }
     else
     {
