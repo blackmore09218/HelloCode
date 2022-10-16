49fda650b56a39848308fd190f4f86be3948a30e
IvanZakharov <blackmore09218@yandex.ru>
2022-10-16 08:04:10 +0300

    Программа проверки четности числа

---
+++ Homework003/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+string a=Console.ReadLine();
+int a1= int.Parse(a);
+if(a1%2==0) Console.WriteLine("Введенное число четное");
+else
+Console.WriteLine("Введенное число нечетное");
+
