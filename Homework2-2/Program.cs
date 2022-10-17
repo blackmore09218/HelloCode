f8eaab7cbed7a0d81be14919ebd9635d5f09a1e4
IvanZakharov <blackmore09218@yandex.ru>
2022-10-17 22:56:47 +0300

    Программа вычисления третьей цифры

---
+++ Homework2-2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ввведите число: ");
+string numberStr = Console.ReadLine();
+int len = numberStr.Length;
+if (len>2)
+{
+    int number = int.Parse(numberStr);
+    Console.Write("Третья цифра введенного числа - ");
+    Console.WriteLine((number/Convert.ToInt32(Math.Pow(10,len-3)))%10);
+}
+else Console.WriteLine("Третьей цифры нет");
