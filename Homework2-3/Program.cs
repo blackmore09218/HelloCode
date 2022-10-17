f8c956bcb7290613847c92c54608964a242f73f6
IvanZakharov <blackmore09218@yandex.ru>
2022-10-17 23:08:18 +0300

    Программа определения выходного дня

---
+++ Homework2-3/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите цифру дня недели: ");
+int day = int.Parse(Console.ReadLine());
+if (day==7 || day ==6) Console.WriteLine("Выходной");
+else Console.WriteLine("Будний день");
